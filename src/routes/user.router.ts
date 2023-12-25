a98f653fa39f6f736f7bfd2167542ba528d96800
Victor <mihamisha68@gmail.com>
2023-12-25 18:26:38 +0200

    api test

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -11,6 +11,7 @@ class UserRouter {
 
   getRouter() {
     const router = express.Router();
+    router.route("/").get(this.userController.getUsersTest);
     router.route("/register").post(this.userController.registerUser);
     router.route("/login").post(this.userController.loginUser);
     router.route("/auth").post(this.userController.authUser);
